26bafccbd4d131d8338d241b50ef9bd6ceca09a6
repu1sion <bearrailgun@gmail.com>
2017-05-06 23:37:33 +0300

    variabled LIBPCAPTRACE_IFACE set in buildrun.sh script

--- buildrun.sh
+++ buildrun.sh
@@ -8,12 +8,15 @@ fi
 
 #make && sudo ldconfig $PWD
 
+export LIBPCAPTRACE_IFACE="enp3s0,odp:03:00.0"
+
 cd /mnt/raw/src/tcpdumplibpcap/libpcapext/libpcap-1.7.4
 cp libpcap.so.1.7.4 /mnt/raw/gdwk/pcap_trace_mapper/libpcap.so
 cd -
 sudo ldconfig $PWD
 rm ptm
-gcc ptm.c -o ptm -L./ -lpcap && sudo ./ptm $IFACE
+gcc ptm.c -o ptm -L./ -lpcap
+sudo -E ./ptm $IFACE
 
 
 # link with common libpcap
